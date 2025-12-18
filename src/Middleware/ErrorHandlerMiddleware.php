a1c7aa5b5c49192d24d75c0404382cbd846c00fd
Leslie Petrimaux <leslypetrimaux@gmail.com>
2025-12-18 09:15:12 +0100

    Implement `error-handler` component

---
+++ src/Middleware/ErrorHandlerMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Waffle\Commons\ErrorHandler\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
+use Waffle\Commons\Contracts\ErrorHandler\ErrorRendererInterface;
+
+/**
+ * Catches all exceptions thrown by subsequent middlewares and renders them using the configured renderer.
+ * This middleware should be the very first one in the stack.
+ */
+final readonly class ErrorHandlerMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private ErrorRendererInterface $renderer
+    ) {}
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            // Execute the rest of the application
+            return $handler->handle($request);
+        } catch (Throwable $e) {
+            // Catch ANY error/exception and delegate rendering
+            return $this->renderer->render($e, $request);
+        }
+    }
+}
---
+++ tests/src/JsonErrorRendererTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WaffleTests\Commons\ErrorHandler;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+use RuntimeException;
+use Waffle\Commons\ErrorHandler\Renderer\JsonErrorRenderer;
+
+final class JsonErrorRendererTest extends TestCase
+{
+    public function testRenderFormatsRfc7807Json(): void
+    {
+        // 1. Setup
+        $factory = $this->createMock(ResponseFactoryInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+        $stream = $this->createStub(StreamInterface::class);
+        $request = $this->createStub(ServerRequestInterface::class);
+        $uri = $this->createStub(UriInterface::class);
+
+        // 2. Expectations
+        $uri->method('getPath')->willReturn('/api/test');
+        $request->method('getUri')->willReturn($uri);
+
+        $response->method('getBody')->willReturn($stream);
+        $response->expects($this->once())
+            ->method('withHeader')
+            ->with('Content-Type', 'application/problem+json')
+            ->willReturnSelf();
+
+        $factory->expects($this->once())
+            ->method('createResponse')
+            ->with(500)
+            ->willReturn($response);
+
+        // 3. Execution
+        $renderer = new JsonErrorRenderer($factory, debug: false);
+        $exception = new RuntimeException('Boom!');
+
+        $renderer->render($exception, $request);
+    }
+}
